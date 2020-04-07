5d00d75e0c26cb0edc2173bbcdcf79716066ee44
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-07 04:36:54 +0000

    Deploy maxfield-dev.nia.ac.cn to github.com/UESTC-Ingress/maxfield-frontend.git:gh-pages

---
+++ precache-manifest.6101fa2bf74909720c7094cd7f0bb6dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77788d098e2d137c9698",
+    "url": "/css/chunk-vendors.a3e420d6.css"
+  },
+  {
+    "revision": "c220e19602e2a000ee619145327ade2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "421887af55e830b1a8bb",
+    "url": "/js/app.36d29b05.js"
+  },
+  {
+    "revision": "77788d098e2d137c9698",
+    "url": "/js/chunk-vendors.3ac99cf4.js"
+  },
+  {
+    "revision": "96d3cf62272c36402b8bc25f34c7c39f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
